4b4acf908bf5ec25a93f6e8e019b9287f13b0bc7
Md. Ifaj Hossan Omi <omiifajhossan@gmail.com>
2024-01-03 02:27:48 +0600

    feat- login and signup created

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+    SelectValue,
+    SelectTrigger,
+    SelectContent,
+    SelectItem,
+    Select,
+} from "@/components/ui/select";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import Cookies from 'js-cookie';
+
+// Then you can use it like this:
+// Cookies.set('name', 'value');
+
+const formSchema = z
+    .object({
+        username: z.string(),
+        password: z.string().min(3),
+    })
+
+export default function Login() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+
+    const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+        // try{
+
+        // }
+        // catch(err){
+        //     console.log(err);
+        // }
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+        });
+        const data = await response.json();
+        console.log(data);
+        console.log(data.accessToken);
+
+        if (response.status === 201) {
+            Cookies.set("accessToken", data.accessToken, { expires: 1, path: "/" });
+            // window.location.href = "/dashboard";
+            // document.cookie = `accessToken=${data.accessToken}; max-age=86400; path=/, HttpOnly=true`;
+            // window.location.href = "/dashboard";
+        } else {
+            alert("Invalid credentials");
+        }
+
+    };
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 ">
+            <Card className="px-10 pt-3">
+                <CardHeader>
+                    <CardTitle>Login</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(handleSubmit)}
+                            className="max-w-md w-full flex flex-col gap-4 mt-5"
+                        >
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => {
+                                    return (
+                                        <FormItem>
+                                            <FormLabel>Username</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="username"
+                                                    type="text"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    );
+                                }}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => {
+                                    return (
+                                        <FormItem>
+                                            <FormLabel>Password</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Password" type="password" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    );
+                                }}
+                            />
+
+                            <Button type="submit" className="w-full">
+                                Submit
+                            </Button>
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
+        </div >
+    );
+}
